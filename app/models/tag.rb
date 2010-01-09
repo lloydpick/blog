3082e698f87c6fc8d83ad3c48eb6f474938b6106
Lloyd Pick <lloydpick@gmail.com>
2010-01-09 18:21:32 +0000

    fixed tags so passenger will start

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -14,12 +14,12 @@
 class Tag < ActiveRecord::Base
 
   # Associations
-  has_permalink :name
+  acts_as_slugable :source_column => :name, :slug_column => :permalink
   has_many :tag_entry
   has_many :posts, :through => :tag_entry
 
   # Validations
-  validates_presence_of :name, :permalink, :description
+  validates_presence_of :name, :description
   validates_uniqueness_of :name, :permalink
 
   named_scope :all, {
