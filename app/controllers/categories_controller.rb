7cfe69225b7199f4bac903cd2245187422d5b414
Lloyd Pick <lloydpick@gmail.com>
2010-01-09 19:37:07 +0000

    removed some commented out code

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -16,20 +16,6 @@ class CategoriesController < ApplicationController
     end
   end
 
-#  def show
-#    @category = Category.find_by_permalink(params[:id]) if params[:id]
-#    @category = Category.find_by_permalink(params[:category_id]) if params[:category_id]
-#    @posts = @category.posts.active if @category
-#    set_title("#{@category.name} Posts")
-#
-#    respond_to do |format|
-#      format.html
-#      format.xml { render :xml => [@category, @posts] }
-#      format.json { render :json => [@category, @posts] }
-#      format.atom
-#    end
-#  end
-
   private
 
   def current_object
