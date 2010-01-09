13bf0fa3f0f51e166e0c5c6c746cf675dacafc26
Lloyd Pick <lloydpick@gmail.com>
2010-01-09 15:36:04 +0000

    added admin routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,18 @@ ActionController::Routing::Routes.draw do |map|
   map.root :controller => "posts", :action => "index"
 
   map.resources :posts, :has_many => :comments
+  map.resources :categories
 
   map.archive_day 'archive/:year/:month/:day', :controller => 'posts', :action => 'archive'
   map.archive_month 'archive/:year/:month', :controller => 'posts', :action => 'archive'
   map.archive_year 'archive/:year', :controller => 'posts', :action => 'archive'
 
-  #map.connect ':controller/:action/:id'
-  #map.connect ':controller/:action/:id.:format'
+  # Admin Routes
+  map.namespace(:admin) do |admin|
+    admin.resources :posts
+    admin.resources :categories
+    admin.resources :comments
+  end
   
   # Needed for pingbacks
   map.connect ':controller/:action/:id.:format'
