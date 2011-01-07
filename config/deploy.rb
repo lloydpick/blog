ee685bcf9e23242292f853c85f10e1a27f4080c2
Lloyd Pick <lloydpick@gmail.com>
2011-01-07 17:46:14 +0000

    deployment update

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,12 @@
-gem 'brightbox', '>=2.3.5'
-require 'brightbox/recipes'
-require 'brightbox/passenger'
+gem 'capistrano-ext', '>=1.2.1'
 
+set :use_sudo, false
 set :application, "blog"
 set :keep_releases, 5
 set :scm_verbose, true
 set :deploy_via, :remote_cache
 set :repository, "git@github.com:lloydpick/blog.git"
 set :scm, :git
-set :local_shared_files, %w(config/database.yml config/initializers/authentication.rb)
 
 ssh_options[:forward_agent] = true
 default_run_options[:pty] = true
@@ -16,49 +14,29 @@ default_run_options[:pty] = true
 set :branch, 'master'
 set :rails_env, 'production'
 
-set :domain, 'limi.co.uk'
-set :domain_aliases, 'www.limi.co.uk,blog.limi.co.uk'
-
 set :user, "limi"
 server "andromeda.limi.co.uk", :app, :web, :db, :primary => true
 
 set(:deploy_to) { File.join("", "home", "limi", "domains", "blog.limi.co.uk") }
 
-namespace :db do
-  desc 'Dumps the production database to db/production_data.sql on the remote server'
-  task :remote_db_dump, :roles => :db, :only => { :primary => true } do
-    run "cd #{deploy_to}/#{current_dir} && " +
-      "rake RAILS_ENV=#{rails_env} db:database_dump --trace"
-  end
+task :after_update_code do
+  run "rm -rf #{release_path}/config/database.yml"
+  run "ln -nfs #{deploy_to}/#{shared_dir}/config/database.yml #{release_path}/config/database.yml"
 
-  desc 'Downloads db/production_data.sql from the remote production environment to your local machine'
-  task :remote_db_download, :roles => :db, :only => { :primary => true } do
-    execute_on_servers(options) do |servers|
-      self.sessions[servers.first].sftp.connect do |tsftp|
-        tsftp.download!("#{deploy_to}/#{current_dir}/db/#{rails_env}_data.sql", "db/#{rails_env}_data.sql")
-      end
-    end
-  end
+  run "rm -rf #{release_path}/config/initializers/authentication.rb"
+  run "ln -nfs #{deploy_to}/#{shared_dir}/config/initializers/authentication.rb #{release_path}/config/initializers/authentication.rb"
 
-  desc 'Cleans up data dump file'
-  task :remote_db_cleanup, :roles => :db, :only => { :primary => true } do
-    execute_on_servers(options) do |servers|
-      self.sessions[servers.first].sftp.connect do |tsftp|
-        tsftp.remove! "#{deploy_to}/#{current_dir}/db/#{rails_env}_data.sql"
-      end
-    end
-  end
-
-  desc 'Dumps, downloads and then cleans up the production data dump'
-  task :remote_db_runner do
-    remote_db_dump
-    remote_db_download
-    remote_db_cleanup
-  end
+  run "rm -rf #{release_path}/config/initializers/rakismet.rb"
+  run "ln -nfs #{deploy_to}/#{shared_dir}/config/initializers/rakismet.rb #{release_path}/config/initializers/rakismet.rb"
 end
 
-Dir[File.join(File.dirname(__FILE__), '..', 'vendor', 'gems', 'hoptoad_notifier-*')].each do |vendored_notifier|
-  $: << File.join(vendored_notifier, 'lib')
+namespace :deploy do
+   task :start do ; end
+   task :stop do ; end
+   task :restart, :roles => :app, :except => { :no_release => true } do
+     run "touch #{File.join(current_path,'tmp','restart.txt')}"
+   end
 end
 
+require 'bundler/capistrano'
 require 'hoptoad_notifier/capistrano'
